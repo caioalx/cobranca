02a0f30f5dd992f15d91b80d1b7d5e9da8757a5c
caio <caio@caio-notebook>
2016-09-10 12:04:29 -0300

    Implementando pesquisa

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
-import com.algaworks.cobranca.repository.Titulos;
+import com.algaworks.cobranca.repository.filter.TituloFilter;
 import com.algaworks.cobranca.service.CadastroTituloService;
 
 @Controller
@@ -25,9 +25,6 @@ import com.algaworks.cobranca.service.CadastroTituloService;
 public class TituloController {
 	
 	private static final String CADASTRO_VIEW = "CadastroTitulo";
-	
-	@Autowired
-	private Titulos titulos;
 	
 	@Autowired
 	private CadastroTituloService cadastroTituloService;
@@ -58,9 +55,9 @@ public class TituloController {
 	}
 	
 	@RequestMapping
-	public ModelAndView pesquisar(){
+	public ModelAndView pesquisar(@ModelAttribute("filtro") TituloFilter filtro){
+		List<Titulo> lista = cadastroTituloService.filtrar(filtro);
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
-		List<Titulo> lista = titulos.findAll();
 		mv.addObject("titulos", lista);
 		return mv;
 	}
--- src/main/java/com/algaworks/cobranca/repository/Titulos.java
+++ src/main/java/com/algaworks/cobranca/repository/Titulos.java
@@ -1,9 +1,13 @@
 package com.algaworks.cobranca.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.cobranca.model.Titulo;
 
 public interface Titulos extends JpaRepository<Titulo, Long> {
 
+	List<Titulo> findByDescricaoContainingIgnoreCase(String descricao);
+	
 }
